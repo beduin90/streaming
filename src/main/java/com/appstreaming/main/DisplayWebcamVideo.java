56913bcb518d76840880998564a8ac1ac25527c7
beduin90 <maciej.niezabitowski@student.uj.edu.pl>
2017-04-25 20:20:16 +0200

    test xuggler camera , preparing to use applet

--- src/main/java/com/appstreaming/main/DisplayWebcamVideo.java
+++ src/main/java/com/appstreaming/main/DisplayWebcamVideo.java
@@ -1,6 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 Xuggle Inc.  All rights reserved.
+ *
+ * This file is part of Xuggle-Xuggler-Main.
+ *
+ * Xuggle-Xuggler-Main is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xuggle-Xuggler-Main is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Xuggle-Xuggler-Main.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
 package com.appstreaming.main;
 
-import javax.swing.JFrame;
 import java.awt.image.BufferedImage;
 
 import com.xuggle.xuggler.ICodec;
@@ -72,8 +90,6 @@ public class DisplayWebcamVideo
     @SuppressWarnings("deprecation")
     public static void main(String[] args)
     {
-
-
         String driverName = "vfwcap";
         String deviceName=  "0";
 
@@ -96,7 +112,7 @@ public class DisplayWebcamVideo
         IMetaData params = IMetaData.make();
 
         params.setValue("framerate", "30/1");
-        params.setValue("video_size", "320x240");
+        params.setValue("video_size", "800x600");
 
         // Open up the container
         int retval = container.open(deviceName, IContainer.Type.READ, format,
